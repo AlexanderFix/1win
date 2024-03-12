a088208aabaf55ea55e66dff0a78afe5f630f62a
Александр <reduik@yandex.ru>
2024-03-13 00:27:54 +0300

    add burger menu

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 
 //Burger
 
-// let burgerList = document.querySelectorAll('.burger_list')
+let burgerList = document.querySelectorAll('.burger_list')
 
-// function menuBurger() {
-//   burger_wrapper.classList.toggle('active_burger')
-//   burger_nav.classList.toggle('active_burger')
-//   document.body.classList.toggle('lock')
-//   burger_menu.classList.toggle('burger_active')
-//   header.classList.toggle('header_active')
-// }
+
+function menuBurger() {
+  burger_wrapper.classList.toggle('active_burger')
+  burger_nav.classList.toggle('active_nav')
+  document.body.classList.toggle('lock')
+  burger_menu.classList.toggle('burger_active')
+ burger_hr.classList.toggle('burger_active')
+}
 
 // burgerList.forEach(item => {
 //   item.addEventListener('click', () => {
@@ -18,11 +19,11 @@
 //     burger_nav.classList.remove('active_burger')
 //     document.body.classList.remove('lock')
 //     burger_menu.classList.remove('burger_active')
-//     header.classList.remove('header_active')
+   
 //   })
 // });
 
-// burger_button.addEventListener('click', menuBurger)
+burger_button.addEventListener('click', menuBurger)
 
 // Burger end
 
