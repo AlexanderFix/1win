65de9aec8ace675743c69653f4f783340bca7bb9
Александр <reduik@yandex.ru>
2024-03-15 22:01:29 +0300

    add review page

---
+++ js/review.js
@@ -0,0 +1,32 @@
+//Burger
+
+let burgerList = document.querySelectorAll('.burger_list')
+
+
+function menuBurger() {
+  burger_wrapper.classList.toggle('active_burger')
+  burger_nav.classList.toggle('active_nav')
+  document.body.classList.toggle('lock')
+  burger_menu.classList.toggle('burger_active')
+ burger_hr.classList.toggle('burger_active')
+}
+
+// burgerList.forEach(item => {
+//   item.addEventListener('click', () => {
+//     burger_wrapper.classList.remove('active_burger')
+//     burger_nav.classList.remove('active_burger')
+//     document.body.classList.remove('lock')
+//     burger_menu.classList.remove('burger_active')
+   
+//   })
+// });
+
+burger_button.addEventListener('click', menuBurger)
+
+// Burger end
+
+
+
+link_site.addEventListener('click', () => {
+    location.href='http://google.com';
+})
